78acffead398bce92e05f8ad9024720b9ddcf267
Penmetsa <satyanarayana.raju@hpe.com>
2017-02-28 19:12:01 +0530

    Restful API URL has changed.

--- src/main/java/com/hpe/calEStore/web/controller/DashboardController.java
+++ src/main/java/com/hpe/calEStore/web/controller/DashboardController.java
@@ -40,7 +40,7 @@ public class DashboardController {
 
 		ObjectMapper mapper = new ObjectMapper();
 		OrderStatisticsDM orderStatisticsDm = mapper.readValue(
-				new URL("http://localhost:8088/calEStore/getOrderStatistics"),
+				new URL("http://localhost:8088/calestore/getOrderStatistics"),
 				OrderStatisticsDM.class);
 
 		mv.addObject("orderStatistics", orderStatisticsDm);
