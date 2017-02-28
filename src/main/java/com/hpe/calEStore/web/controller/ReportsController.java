61d6e4af4100551a348537c2d18db4edac01163e
Bikkina <sureshbabu@hpe.com>
2017-02-28 23:02:14 +0530

    Base artifacts added for swagger ui integration

--- src/main/java/com/hpe/calEStore/web/controller/ReportsController.java
+++ src/main/java/com/hpe/calEStore/web/controller/ReportsController.java
@@ -1,5 +1,8 @@
 package com.hpe.calEStore.web.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,12 +13,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.hpe.calEStore.model.OrderStatisticsDM;
 import com.hpe.calEStore.service.OrderReportService;
 
+@Api(value = "Order Statistics")
 @Controller
 public class ReportsController {
 
 	@Autowired
 	private OrderReportService orderReportService;
 
+	
+	@ApiOperation(value = "Fetch Order Statistics")
 	@RequestMapping(value = "/getOrderStatistics", method = RequestMethod.GET)
 	public ResponseEntity<OrderStatisticsDM> getOrderStatistics() {
 
